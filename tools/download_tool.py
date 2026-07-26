e020c7a2a878a9b20447b30732b4df24ac9c267e
cybervisi0n <112425559+cybervisi0n@users.noreply.github.com>
2026-07-25 19:19:38 -0500

    Setup meson build

--- tools/download_tool.py
+++ tools/download_tool.py
@@ -79,7 +79,7 @@ def sjiswrap_url(tag: str) -> str:
 
 def wibo_url(tag: str) -> str:
     repo = "https://github.com/decompals/wibo"
-    return f"{repo}/releases/download/{tag}/wibo"
+    return f"{repo}/releases/download/{tag}/wibo-x86_64"
 
 
 def orthrus_url(tag: str) -> str:
