488c51f14a28206f1763ad54ef775ecf5b2351c7
cybervisi0n <112425559+cybervisi0n@users.noreply.github.com>
2026-08-01 21:13:20 -0500

    attempt to fix CI build

--- scripts/ci_build_win.sh
+++ scripts/ci_build_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+wget https://archive.org/download/animal-crossing-usa/Animal%20Crossing%20%28USA%29.iso -O orig/GAFE01_00/GAFE01_00.iso
 ./setup_build_win64.sh
 meson compile -C build_win64
